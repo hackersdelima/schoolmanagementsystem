57a538e3753745917ac3d0915265663149ba048d
shishir <shishir4913120@gmail.com>
2017-12-30 14:55:26 +0545

    #user login managed

--- schoolmanagementsystem/src/com/school/dbconnection/DbConnection.java
+++ schoolmanagementsystem/src/com/school/dbconnection/DbConnection.java
@@ -5,25 +5,21 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class DbConnection {
-	 static Connection conn=null;
-	 static Statement stmt=null;
-	public DbConnection(){
-		try{
-			Class.forName("com.mysql.jdbc.Driver");	
-		}
-		catch(Exception e){
-			System.out.println(e);
-		}
-	}
-	public static Connection dbconnection(String database){
-		try{
-			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/"+database+"","root","");
-			stmt=conn.createStatement();
-			System.out.println(database+" connected");
-		}
-		catch (Exception e){
-			System.out.println("database error"+e);
-		}
-		return conn;
-	}
+	private static Connection conn=null;
+    static{//instantiation block-called before constructor static-once work is done remains constant
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection(){
+        try{
+            conn=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/aranikodb","root","");//javproject name of database
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return conn;
+    }
 }
---
+++ schoolmanagementsystem/src/com/school/model/UserModel.java
@@ -0,0 +1,37 @@
+package com.school.model;
+
+public class UserModel {
+String userid, username, password, status;
+
+public String getUserid() {
+	return userid;
+}
+
+public void setUserid(String userid) {
+	this.userid = userid;
+}
+
+public String getUsername() {
+	return username;
+}
+
+public void setUsername(String username) {
+	this.username = username;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+public String getStatus() {
+	return status;
+}
+
+public void setStatus(String status) {
+	this.status = status;
+}
+}
